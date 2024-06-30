5584faa718234a61cb04edaa6f44cd222b8fefa1
frestylek <42667570+frestylek@users.noreply.github.com>
2024-07-01 01:45:24 +0200

    Create downloadernbs.lua

---
+++ downloadernbs.lua
@@ -0,0 +1,19 @@
+local answer 
+repeat
+   io.write("Podaj link grubasie? ")
+   io.flush()
+   answer=io.read()
+until answer~= nil
+local name
+repeat
+   io.write("Podaj nazwe? ")
+   io.flush()
+   name=io.read()
+until name~= nil
+ 
+if answer ~= nil and name ~= nil then
+  local w = http.get(answer,nil,nil)
+  test = fs.open(name .. ".nbs","wb")
+  test.write(w.readAll())
+  test.close()
+end
